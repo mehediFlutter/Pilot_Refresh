478666b664efd87cf1036300ddfd017c14f514a8
mehediFlutter <mehedii.edu.be@gmail.com>
2023-11-25 18:24:36 +0600

    products

---
+++ lib/widget/bottom_nav_base-screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:pilot_bazar/screens/double_vehicle_screen.dart';
+import 'package:pilot_bazar/screens/home_page.dart';
+
+
+class BottomNavBaseScreen extends StatefulWidget {
+   BottomNavBaseScreen({super.key});
+
+  @override
+  State<BottomNavBaseScreen> createState() => _BottomNavBaseScreenState();
+}
+
+class _BottomNavBaseScreenState extends State<BottomNavBaseScreen> {
+
+  int _selectedScreenIndex= 0 ;
+  final List<Widget> _screens =  [
+  HomeVehicle(),
+  DoublVehicle(),
+  HomeVehicle(),
+  HomeVehicle(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: _screens[_selectedScreenIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedScreenIndex,
+          unselectedItemColor: Colors.grey,
+
+          unselectedLabelStyle: const TextStyle(
+            color: Colors.grey,
+          ),
+          showUnselectedLabels: true,
+          selectedItemColor: Colors.green,
+        onTap: (int index) {
+          _selectedScreenIndex = index;
+          if(mounted){
+            setState(() {
+              
+            });
+          }
+        },
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home",),
+        BottomNavigationBarItem(icon: Icon(Icons.view_agenda_outlined),label: "Double vehicle"),
+        BottomNavigationBarItem(icon: Icon(Icons.cancel),label: "Cancel"),
+        BottomNavigationBarItem(icon: Icon(Icons.done_all),label: "Completed"),
+      ],
+    ));
+  }
+}
